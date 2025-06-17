aaba4ab0ea8afc50b31006d93808bb0e25add72f
chinnayyachintha <chinthayadav6@gmail.com>
2025-06-17 21:29:56 +0530

    add another account

--- terraform/account-requests.tf
+++ terraform/account-requests.tf
@@ -184,3 +184,40 @@ module "account_request_05" {
 
   account_customizations_name = "SANDBOX"
 }
+
+module "account_request_06" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "chinnayya+aws@gmail.com"
+    AccountName               = "AFT-ACCOUNT-06"
+    ManagedOrganizationalUnit = "Sandbox" 
+    SSOUserEmail              = "chinnayya+aws@gmail.com"
+    SSOUserFirstName          = "Chinna"
+    # Replace with your actual last name
+    SSOUserLastName           = "Chinna"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "chinnayya+aws@gmail.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AWS Control Tower Lab"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "SANDBOX"
+}
